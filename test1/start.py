e90f95b24f6e28c91cc3ef7eca2037b2fd5d469a
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-10-17 13:47:59 -0300

    Inserted code snippet 1 on `test1/start.py`
    Code snippet 1:
    
    ```python
    import pkgutil
    search_path = ['.']  # set to None to see all modules importable from sys.path
    all_modules = [x[1] for x in pkgutil.iter_modules(path=search_path)]
    print(all_modules)
    ```

---
+++ test1/start.py
@@ -0,0 +1,4 @@
+import pkgutil
+search_path = ['.']  # set to None to see all modules importable from sys.path
+all_modules = [x[1] for x in pkgutil.iter_modules(path=search_path)]
+print(all_modules)
