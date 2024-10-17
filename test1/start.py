863d947e021f86b5a37de6ee2ea10c06c7823f03
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-10-17 14:31:51 -0300

    Adjusted code snippet `importablemodules` to print a label before the available modules

--- test1/start.py
+++ test1/start.py
@@ -1,4 +1,5 @@
 import pkgutil
 search_path = ['.']  # set to None to see all modules importable from sys.path
 all_modules = [x[1] for x in pkgutil.iter_modules(path=search_path)]
+print("Available modules to import: ")
 print(all_modules)
