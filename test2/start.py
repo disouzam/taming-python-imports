20805e9b6b5dfa15ac7d2b36252fa0b081c3893b
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-10-17 22:21:26 -0300

    Changed snippet in `test2/start.py` to print an empty all_modules

--- test2/start.py
+++ test2/start.py
@@ -1,10 +1,13 @@
 import sys
 import pkgutil
 
+
 search_path = ['.']  # set to None to see all modules importable from sys.path
 all_modules = [x[1] for x in pkgutil.iter_modules(path=search_path)]
 print("Available modules to import: ")
 nmodules = 0
+if len(all_modules) == 0:
+    print(all_modules)
 for module in all_modules:
     nmodules += 1
     print(f"{nmodules}: Module name: {module}")
