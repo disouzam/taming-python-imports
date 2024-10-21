ae21ef9efa388fd4b0a1ef189056592146aa67fc
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-10-21 12:33:35 -0300

    Set of experimental changes, not so conclusive or clear of the best strategy for imports so far
    - What is clearer, though, is the configurations for Pylint in the environment and also for VS Code settings:
    
        pylint.importStrategy: fromEnvironment,
        pylint.path: [
            /.venv/python,
            -m,
            pylint
        ]

--- test2/start.py
+++ test2/start.py
@@ -1,25 +1,11 @@
-import sys
-import pkgutil
+"""start.py"""
 from custom_math import func_math
 
 
-search_path = ['.']  # set to None to see all modules importable from sys.path
-all_modules = [x[1] for x in pkgutil.iter_modules(path=search_path)]
-print("Available modules to import: ")
-nmodules = 0
-if len(all_modules) == 0:
-    print(all_modules)
-for module in all_modules:
-    nmodules += 1
-    print(f"{nmodules}: Module name: {module}")
+def main() -> None:
+    """Entry point"""
+    func_math()
 
 
-print("Paths in sys.path:")
-npaths = 0
-for pathitem in sys.path:
-    npaths += 1
-    print(f"{npaths}: Path: {pathitem}")
-
-print("\n\n")
-
-func_math()
+if __name__ == '__main__':
+    main()
